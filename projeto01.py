b61cad1fbf5e50a766e5c8cf11859bfbf2e5e8b5
Freesz0 <matheusfreesz1@gmail.com>
2024-09-23 15:58:03 -0300

    02 09 24 init

--- projeto01.py
+++ projeto01.py
@@ -3,28 +3,93 @@ class Carro:
     modelo = ""
     ano = 0
     velocidade_atual = 0
-    estado = "desligado"
+    ligado = False
+
+    def get_marca(self):
+        return self.marca
+
+    def set_marca(self, marca):
+        self.marca = marca
+
+    def get_modelo(self):
+        return self.modelo
+
+    def set_modelo(self, modelo):
+        self.modelo = modelo
+
+    def get_ano(self):
+        return self.ano
+
+    def set_ano(self, ano):
+        if ano < 0:
+            print("Ano inválido.")
+        else:
+            self.ano = ano
+
+    def get_velocidade_atual(self):
+        return self.velocidade_atual
+
+    def set_velocidade_atual(self, velocidade_atual):
+        if velocidade_atual < 0:
+            print("Velocidade não pode ser menor que 0.")
+        else:
+            self.velocidade_atual = velocidade_atual
+
+    def get_ligado(self):
+        return self.ligado
+
+    def set_ligado(self, ligado):
+        self.ligado = ligado
+
+    def acelerar(self, quantidade):
+        if self.ligado:
+            self.velocidade_atual += quantidade
+        else:
+            print("Não é possível acelerar o carro desligado.")
 
     def ligar(self):
-        self.estado = "ligado"
+        if not self.ligado:
+            self.ligado = True
+            print(self.modelo,"ligado.")
+        else:
+            print("Este", self.modelo, "ja esta ligado.")
+
 
     def desligar(self):
-        self.estado = "desligado"
+        if self.ligado:
+            self.ligado = False
+            self.velocidade_atual = 0
+            print(self.modelo, "desligado.")
+        else:
+            print("Este", self.modelo,"ja esta desligado.")
 
-carro = Carro()
-carro.marca = "Fiat"
-carro.modelo = "Marea"
-print(carro.marca)
-carro.ligar()
-print(carro.estado)
-carro.desligar()
-print(carro.estado)
+    def frear(self, quantidade):
+        if self.ligado:
+            print(self.modelo, "desligado, impossivel frear.")
+        else:
+            if quantidade > self.velocidade_atual:
+                print("Frenagem invalida")
+            else:
+                self.velocidade_atual -= quantidade
+
+
+carro0 = Carro()
+carro0.marca = "Fiat"
+carro0.modelo = "Marea"
+print(carro0.marca)
+print(carro0.modelo)
+carro0.ligar()
+carro0.ligar()
+print(carro0.velocidade_atual)
+carro0.desligar()
+carro0.desligar()
 
 carro1 = Carro ()
 carro1.marca = "Peugeot"
 carro1.modelo = "206"
 print(carro1.marca)
+print(carro1.modelo)
 carro1.ligar()
-print(carro1.estado)
+carro1.ligar()
 carro1.desligar()
-print(carro1.estado)
+carro1.desligar()
