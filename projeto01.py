0062d42b8adb02e9c989a85a86b7983e6cfcbfc7
Freesz0 <matheusfreesz1@gmail.com>
2024-10-03 19:11:33 -0300

    03 10 2024

--- projeto01.py
+++ projeto01.py
@@ -1,10 +1,31 @@
+class Marca:
+    def __init__(self):
+        self.nome = ""
+
+    def get_nome(self):
+        return self.nome
+
+    def set_nome(self, nome):
+        self.nome = nome
+
+class Modelo:
+    def __init__(self):
+        self.nome = ""
+
+    def get_nome(self):
+        return self.nome
+
+    def set_nome(self, nome):
+        self.nome = nome
+
 class Carro:
 
-    marca = ""
-    modelo = ""
-    ano = 0
-    velocidade_atual = 0
-    ligado = False
+    def __init__(self):
+        self.marca = None
+        self.modelo = None
+        self.ano = 0
+        self.velocidade_atual = 0
+        self.ligado = False
 
     def get_marca(self):
         return self.marca
@@ -28,7 +49,7 @@ class Carro:
             self.ano = ano
 
     def get_velocidade_atual(self):
-        return self.velocidade_atual
+        return f"{self.velocidade_atual} Km/h"
 
     def set_velocidade_atual(self, velocidade_atual):
         if velocidade_atual < 0:
@@ -51,56 +72,79 @@ class Carro:
     def ligar(self):
         if not self.ligado:
             self.ligado = True
-            print(self.modelo,"ligado.")
+            print(self.get_nome_marca(),self.get_nome_modelo(),"ligado.")
         else:
-            print("Este", self.modelo, "ja esta ligado.")
-
+            print("Este", self.get_nome_modelo(), "ja esta ligado.")
 
     def desligar(self):
         if self.ligado:
             self.ligado = False
             self.velocidade_atual = 0
-            print(self.modelo, "desligado.")
+            print(self.get_nome_marca(),self.get_nome_modelo(), "desligado.")
         else:
-            print("Este", self.modelo,"ja esta desligado.")
+            print("Este", self.get_nome_modelo(),"ja esta desligado.")
 
     def frear(self, quantidade):
         if not self.ligado:
-            print(self.modelo, "desligado, impossivel frear.")
+            print(self.get_nome_modelo(), "desligado, impossivel frear.")
         else:
             if quantidade > self.velocidade_atual:
                 print("Frenagem invalida")
             else:
                 self.velocidade_atual -= quantidade
 
+    def get_nome_marca(self):
+        if self.marca is None:
+            print("Carro sem marca definida")
+        else:
+            return self.marca.get_nome()
+
+    def get_nome_modelo(self):
+        if self.modelo is None:
+            print("Carro sem modelo definido")
+        else:
+            return self.modelo.get_nome()
+
+marca0 = Marca()
+marca0.set_nome("Fiat")
+
+modelo0 = Modelo()
+modelo0.set_nome("Marea")
 
 carro0 = Carro()
-carro0.marca = "Fiat"
-carro0.modelo = "Marea"
-print(carro0.marca)
-print(carro0.modelo)
+carro0.set_marca(marca0)
+carro0.set_modelo(modelo0)
+print(carro0.get_nome_marca())
+print(carro0.get_nome_modelo())
 carro0.ligar()
 carro0.acelerar(40)
 carro0.acelerar(20)
-print(carro0.velocidade_atual)
+print(carro0.get_velocidade_atual())
 carro0.frear(30)
-print(carro0.velocidade_atual)
+print(carro0.get_velocidade_atual())
 carro0.frear(30)
-print(carro0.velocidade_atual)
+print(carro0.get_velocidade_atual())
 carro0.desligar()
+print()
+
+marca1 = Marca()
+marca1.set_nome("Koenigsegg")
+
+modelo1 = Modelo()
+modelo1.set_nome("Jesko")
 
 carro1 = Carro ()
-carro1.marca = "Peugeot"
-carro1.modelo = "206"
-print(carro1.marca)
-print(carro1.modelo)
+carro1.set_marca(marca1)
+carro1.set_modelo(modelo1)
+print(carro1.get_nome_marca())
+print(carro1.get_nome_modelo())
 carro1.ligar()
-carro1.acelerar(70)
-carro1.acelerar(20)
-print(carro1.velocidade_atual)
-carro1.frear(50)
-print(carro1.velocidade_atual)
-carro1.frear(40)
-print(carro1.velocidade_atual)
+carro1.acelerar(110)
+carro1.acelerar(50)
+print(carro1.get_velocidade_atual())
+carro1.frear(90)
+print(carro1.get_velocidade_atual())
+carro1.frear(70)
+print(carro1.get_velocidade_atual())
 carro1.desligar()
 carro1.frear(30)
